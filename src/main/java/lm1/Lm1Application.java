67464b22ede5ba7b485f6ad0c000c597bf8f245b
Matt Cadorette <mcadorette@gmail.com>
2018-11-28 10:41:37 -0500

    Initial commit from Atomist

---
+++ src/main/java/lm1/Lm1Application.java
@@ -0,0 +1,12 @@
+package lm1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lm1Application.class, args);
+	}
+}
---
+++ src/test/java/lm1/Lm1ApplicationTests.java
@@ -0,0 +1,16 @@
+package lm1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Lm1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
